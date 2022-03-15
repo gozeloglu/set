ee55df4b1c79f0a50ee0c5dce2b2aad24eb72459
Gökhan Özeloğlu <gozeloglu@gmail.com>
2022-03-15 21:32:33 +0300

    Create set_bench_test.go for benchmark tests
    * Test added for New() function.
    
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- set_test.go
+++ set_test.go
@@ -2,7 +2,18 @@ package set
 
 import (
 	"testing"
+	"unsafe"
 )
+
+func TestSet_New(t *testing.T) {
+	s := New()
+	if len(s.set) != 0 {
+		t.Errorf("size is not zero. it is %v", len(s.set))
+	}
+	if unsafe.Sizeof(setVal) != 0 {
+		t.Errorf("setVal size is not zero. it is %v", unsafe.Sizeof(setVal))
+	}
+}
 
 func TestSet_Add(t *testing.T) {
 	testCases := []struct {
@@ -1019,55 +1030,5 @@ func TestSet_SymmetricDifference(t *testing.T) {
 				}
 			}
 		})
-	}
-}
-
-var benchmarkData = []interface{}{
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-	true, false,
-	"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n",
-	1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}
-
-func BenchmarkSet_Add(b *testing.B) {
-	s := New()
-	for i := 0; i < b.N; i++ {
-		s.Add(i)
-	}
-}
-
-func BenchmarkSet_Append(b *testing.B) {
-	s := New()
-	for i := 0; i < b.N; i++ {
-		s.Append(benchmarkData...)
-	}
-}
-
-func BenchmarkSet_Remove(b *testing.B) {
-	s := New()
-	for i := 0; i < b.N; i++ {
-		s.Add(i)
-		s.Remove(i)
-	}
-}
-
-func BenchmarkSet_Contains(b *testing.B) {
-	s := New()
-	idx := 0
-	n := len(benchmarkData)
-	s.Append(benchmarkData...)
-	for i := 0; i < b.N; i++ {
-		s.Contains(i)
-		idx++
-		if idx == n {
-			idx = 0
-		}
-	}
-}
-
-func BenchmarkSet_Clear(b *testing.B) {
-	s := New()
-	s.Append(benchmarkData...)
-	for i := 0; i < b.N; i++ {
-		s.Clear()
 	}
 }
