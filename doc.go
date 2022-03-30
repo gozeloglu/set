91bf4abaa4400812101da919a52927f82b1c2091
Gökhan Özeloğlu <gozeloglu@gmail.com>
2022-03-30 22:05:13 +0300

    Update doc.go
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- doc.go
+++ doc.go
@@ -2,34 +2,10 @@
 Package set is a set package provides set data structure for Go. It is written
 without any dependency.
 
-	package main
-
-	import (
-		"fmt"
-		"github.com/gozeloglu/set"
-	)
-
-	func main() {
-		s := set.New(set.ThreadUnsafe)
-		s.Add(123)
-
-		exist := s.Contains(123)
-		if !exist {
-			fmt.Println("123 not exist")
-		}
-
-		s.Append(1, 2, 3, 4, "abc")    // Add multiple values
-		values := []interface{}{"github", 100, 640, 0.43, false}
-		s.Append(values...) // Append the array of elements
-
-		s.Remove(4)
-		size := s.Size()
-		fmt.Println(size)   // Prints 5
-
-		s.Pop()    // Returns random value from the set
-		s.Clear()
-		fmt.Println(s.Size())   // Prints 0
-	}
+	// Create new thread-unsafe set
+	unsafeSet := set.New(set.ThreadUnsafe)
+	// Create new thread-safe set
+	safeSet := set.New(set.ThreadSafe)
 
 You can call the Union() method for creating a new Set which contains the all
 items from the set1 and set2. It concatenates two sets and creates a new one.
